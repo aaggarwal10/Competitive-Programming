3d4e329c2068670d57f444b07e9be5512aa5d492
Anish Aggarwal <anish.5@hotmail.com>
2018-09-05 18:49:30 -0400

    Update S3 Gates.py

--- CCC/2015 Senior CCC/S3 Gates.py
+++ CCC/2015 Senior CCC/S3 Gates.py
@@ -1,22 +1,39 @@
-import sys
-gates = int(sys.stdin.readline())
-planes = int(sys.stdin.readline())
+#include <iostream>
 
-airport = [0 for x in range(gates)]
-result = 0
-
-for x in range(planes):
-  plane = int(sys.stdin.readline()) - 1
-    
-  while plane >= 0 and airport[plane] > 0:
-    save = airport[plane]
-    airport[plane] += 1
-    plane = plane - save
-  
-  if plane < 0:
-    break
-  else:
-    airport[plane] += 1
-    result += 1
-    
-print(result)
+using namespace std;
+int G,P;
+int gates[100001];
+int counter = 0;
+int isAvaillable(int x){
+    if (gates[x] == x){
+        return x;
+    }
+    else{
+        gates[x] = isAvaillable(gates[x]);
+        return gates[x];
+    }
+}
+int main()
+{
+    cin.sync_with_stdio(false);
+    cin.tie(0);
+    cin >> G >> P;
+    for(int i = 0; i<100001; i++){
+        gates[i] = i;
+    }
+    for(int i = 0; i<P; i++){
+        int g;
+        cin >> g;
+        int firstGate = isAvaillable(g);
+        //cout << firstGate << endl;
+        if(firstGate>0){
+            counter += 1;
+            gates[firstGate] = isAvaillable(firstGate-1); // find next gate
+        }
+        else{
+            break;
+        }
+    }
+    cout << counter << endl;
+    return 0;
+}
